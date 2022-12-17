83c35059f1be207008398acb8ca4dac747e3fcc4
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2022-12-17 21:38:04 +0530

    Added sounds for keys

--- PianoMe/ViewController.swift
+++ PianoMe/ViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     var scale: Int = 3;
+    var player: AVAudioPlayer!
     
     // MARK: Outlets
     @IBOutlet weak var lblScale: UITextField!
@@ -19,6 +21,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnDown: UIButton!
     
     // MARK: Actions
+    @IBAction func onKeyPress(_ sender: UIButton) {
+//        print(sender.titleLabel?.text!);
+        playSound(key: (sender.titleLabel?.text)!);
+    }
+    
     @IBAction func onBtnUpClick(_ sender: UIButton) {
         scale += 1;
         btnDown.isEnabled = true;
@@ -31,7 +38,7 @@ class ViewController: UIViewController {
     @IBAction func OnBtnDownClick(_ sender: UIButton) {
         scale -= 1;
         btnUp.isEnabled = true;
-        if(scale == 0) {
+        if(scale == 1) {
             btnDown.isEnabled = false;
         }
         lblScale.text = String(scale);
@@ -43,6 +50,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         lblScale.text = String(scale);
+    }
+    
+    // MARK: Functions
+    func playSound(key: String) {
+        let soundResource: String = key + String(scale);
+        let url = Bundle.main.url(forResource: soundResource, withExtension: "mp3");
+        player = try! AVAudioPlayer(contentsOf: url!);
+        player.play();
     }
 }
 
