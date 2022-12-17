bae65534a624bbc7665b574bbd3669305255c127
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2022-12-17 20:12:31 +0530

    Added functionality to change scale

--- PianoMe/ViewController.swift
+++ PianoMe/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var scale: Int = 3;
+    
+    // MARK: Outlets
+    @IBOutlet weak var lblScale: UITextField!
+    
+    @IBOutlet weak var btnUp: UIButton!
+    
+    @IBOutlet weak var btnDown: UIButton!
+    
+    // MARK: Actions
+    @IBAction func onBtnUpClick(_ sender: UIButton) {
+        scale += 1;
+        btnDown.isEnabled = true;
+        if(scale == 7) {
+            btnUp.isEnabled = false;
+        }
+        lblScale.text = String(scale);
+    }
+    
+    @IBAction func OnBtnDownClick(_ sender: UIButton) {
+        scale -= 1;
+        btnUp.isEnabled = true;
+        if(scale == 0) {
+            btnDown.isEnabled = false;
+        }
+        lblScale.text = String(scale);
+    }
+    
+    
+    // MARK: Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblScale.text = String(scale);
     }
-
-
 }
 
